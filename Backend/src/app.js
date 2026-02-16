a828ad55f95eb6ba05dea3d985030907c816d146
dheeraj <dheerajverma2903@email.com>
2026-02-16 14:11:44 +0530

    Deploy Path fix

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -37,7 +37,7 @@ app.use(express.static(path.join(__dirname, '../public')))
 //Using Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/chat", chatRoutes);
-app.get("/*", (req, res) => {
+app.get("/{*any}", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"))
 })
 
